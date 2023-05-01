6f67cb9d3d8539d32e3ddfe54a96cdedd7410479
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-05-01 14:43:58 +0800

    feat：更新Auth FetchAPI 規則

--- KIWI_ReadCard/Form2.cs
+++ KIWI_ReadCard/Form2.cs
@@ -12,6 +12,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Configuration;
 using Newtonsoft.Json;
+using KIWI_ReadCard.fetchApi;
 
 
 
@@ -29,62 +30,26 @@ namespace KIWI_ReadCard
         private void Form2_Load(object sender, EventArgs e)
         {
 
-        }
+        }        
 
-        static async Task Login(string username, string password)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            LoginForm LoginForm = new LoginForm { username = username ,password=password};
-            string BACKEND_URL = ConfigurationManager.AppSettings["BACKEND_URL"];
-            // 建立 HttpClient 實例
-            var client = new HttpClient();
-
-            // 設定請求的標頭，指定傳遞的資料格式為 JSON
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            // 將 JSON 資料轉換為 HttpContent 物件
-            var content = new StringContent(JsonConvert.SerializeObject(LoginForm), Encoding.UTF8, "application/json");
-
-            // 發送 HTTP POST 請求
-            var response = await client.PostAsync(BACKEND_URL + "auth/login", content);
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            if (response.IsSuccessStatusCode)
+            Auth.AuthLoginApiType.LoginForm UserData = new Auth.AuthLoginApiType.LoginForm
             {
-                
-                SuccessfulMessage SuccessfulMessage = JsonConvert.DeserializeObject<SuccessfulMessage>(responseContent);//反序列化
-                Form1.instance.setToken(SuccessfulMessage.token);
-                Form1.instance.addLog(SuccessfulMessage.message+",登入成功");
-                Form1.instance.addDepartmentComboBox1();
+                username = textBox1_username.Text,
+                password = textBox2_password.Text
+            };
+            Auth.AuthLoginApiType.AuthLoginApiResponse AuthApiResponseData = await Auth.AuthLogin(UserData);
+            if (AuthApiResponseData.status == 200)
+            {
+                Form1.instance.setToken(AuthApiResponseData.token);
+                Form1.instance.addLog(AuthApiResponseData.message + ",登入成功");
                 Form1.instance.closeForm2();
             }
             else
             {
-                Console.WriteLine($"HTTP POST 請求失敗，狀態碼為 {responseContent}");
-                ErrorMessage message = JsonConvert.DeserializeObject<ErrorMessage>(responseContent);//反序列化
-                MessageBox.Show(message.message, "登入失敗", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(AuthApiResponseData.message, "登入失敗", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string username = textBox1_username.Text;
-            string password = textBox2_password.Text;
-            Login(username, password);
-        }
-
-        private class LoginForm{
-            public string username;
-            public string password;
-        }
-
-        private class ErrorMessage {
-            public string message;
-        }
-
-        private class SuccessfulMessage {
-            public string message;
-            public string token;
-        }
-
     }
 }
--- KIWI_ReadCard/fetchApi/Auth.cs
+++ KIWI_ReadCard/fetchApi/Auth.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +12,70 @@ namespace KIWI_ReadCard.fetchApi
 {
     internal class Auth
     {
+        public static async Task<AuthLoginApiType.AuthLoginApiResponse> AuthLogin(AuthLoginApiType.LoginForm UserData)
+        {
+            string BACKEND_URL = ConfigurationManager.AppSettings["BACKEND_URL"];
+            // 建立 HttpClient 實例
+            var client = new HttpClient();
+
+            // 設定請求的標頭，指定傳遞的資料格式為 JSON
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // 將 JSON 資料轉換為 HttpContent 物件
+            var content = new StringContent(JsonConvert.SerializeObject(UserData), Encoding.UTF8, "application/json");
+
+            // 發送 HTTP POST 請求
+            var response = await client.PostAsync(BACKEND_URL + "auth/login", content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                AuthLoginApiType.SuccessfulMessage SuccessfulMessage = JsonConvert.DeserializeObject<AuthLoginApiType.SuccessfulMessage>(responseContent);//反序列化
+                AuthLoginApiType.AuthLoginApiResponse ResponseData = new AuthLoginApiType.AuthLoginApiResponse
+                {
+                    status = 200,
+                    token = SuccessfulMessage.token,
+                    message = SuccessfulMessage.message
+                };
+
+                return ResponseData;
+            }
+            else
+            {
+                AuthLoginApiType.ErrorMessage message = JsonConvert.DeserializeObject<AuthLoginApiType.ErrorMessage>(responseContent);//反序列化
+                AuthLoginApiType.AuthLoginApiResponse ResponseData = new AuthLoginApiType.AuthLoginApiResponse
+                {
+                    status = 400,
+                    message = message.message
+                };
+                return ResponseData;
+
+            }
+        }
+
+        public class AuthLoginApiType
+        {
+            public class LoginForm
+            {
+                public string username;
+                public string password;
+            }
+            public class SuccessfulMessage
+            {
+                public string message;
+                public string token;
+            }
+            public class ErrorMessage
+            {
+                public string message;
+            }
+
+            public class AuthLoginApiResponse
+            {
+                public int status;
+                public string message;
+                public string token;
+            }
+        }
     }
 }
